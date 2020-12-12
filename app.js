60dcc59cd7b7916f090990c5f8cdd44c748a579a
Douglas Naphas <douglasnaphas@gmail.com>
2020-12-12 15:57:28 -0500

    Redirect to /kleenex slides

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
+app.get("/kleenex", () => {
+  return res.redirect(
+    301,
+    "https://docs.google.com/presentation/d/1KnfT8yb825nH4mXQ7E-iypnjumT6pP7SQ3ix6f-fUE4/edit?usp=sharing"
+  );
+});
+
+app.get("/", function (req, res) {
+  return res.send({
+    Output: "This is dougnaphas.com",
   });
 });
 
-app.post('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
+app.post("/", function (req, res) {
+  return res.send({
+    Output: "This is dougnaphas.com",
   });
 });
-
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
-module.exports = app
+module.exports = app;
